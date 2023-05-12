240e3ebb3c26a12ad792aacaeaced4d909a03afe
NYFT <39836503+NY-FT@users.noreply.github.com>
2023-05-12 09:51:19 -0300

    Create database.sql

---
+++ database/database.sql
@@ -0,0 +1,40 @@
+PRAGMA SYNCHRONOUS = OFF;
+
+--
+-- CREATE TABLE
+--
+
+CREATE TABLE `ACCOUNTS` (
+    `ID`                INTEGER         NOT NULL,
+    `NAME`              TEXT            NOT NULL COLLATE NOCASE,
+    `PASSWORD`          TEXT            NOT NULL,
+    `MONEY`             INTEGER         NOT NULL DEFAULT 500,
+    `SCORE`             INTEGER         NOT NULL DEFAULT 0,
+    `SKIN_ID`           INTEGER         NOT NULL,
+    `INTERIOR_ID`       INTEGER         NOT NULL DEFAULT 0,
+    `WORLD_ID`          INTEGER         NOT NULL DEFAULT 0,
+    `WANTED_LEVEL`      INTEGER         NOT NULL DEFAULT 0,
+    `HEALTH`            REAL            NOT NULL DEFAULT 100.0,
+    `ARMOUR`            REAL            NOT NULL DEFAULT 0.0,
+    `JOB_ID`            INTEGER         NOT NULL DEFAULT 0,
+    `ADMIN_LEVEL`       INTEGER         NOT NULL DEFAULT 0,
+    `HUNGER`            REAL            NOT NULL DEFAULT 100.0,
+    `THIRST`            REAL            NOT NULL DEFAULT 100.0,
+    `ENERGY`            REAL            NOT NULL DEFAULT 100.0,
+    `X`                 REAL            NOT NULL DEFAULT 0.0,
+    `Y`                 REAL            NOT NULL DEFAULT 0.0,
+    `Z`                 REAL            NOT NULL DEFAULT 0.0,
+    `A`                 REAL            NOT NULL DEFAULT 0.0,
+    `CREATED_AT`        TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `UPDATED_AT`        TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`ID` AUTOINCREMENT)
+);
+
+--
+-- INDEX
+--
+
+CREATE INDEX 
+    `INDEX_ACCOUNT_NAME` 
+ON 
+    `ACCOUNTS` (`NAME`);
